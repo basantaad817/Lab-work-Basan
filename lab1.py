41ba546131c5d66e7a82d8dfa8893582768ad540
Basan <basantaad817@gmail.com>
2021-12-01 12:27:52 +0545

    lab 1 added

---
+++ lab1.py
@@ -0,0 +1,11 @@
+#printing sum of three numbers
+
+
+# taking input
+num1= int(input("Enter first number"))
+num2= int(input("Enter second number"))
+num3= int(input("Enter third number"))
+
+num=num1 + num2 +num3
+
+print("The sum of those three numbers = ",num)
