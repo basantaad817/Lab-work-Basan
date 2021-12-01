556d68204c4cdd71f4474b468d9872928d6e406b
Basan <basantaad817@gmail.com>
2021-12-01 12:41:27 +0545

    lab 2 done

---
+++ lab2.py
@@ -0,0 +1,7 @@
+# area of triangle
+
+base = int(input("Enter the base of a triangle \n"))
+height = int(input("Enter the height of a triangle \n"))
+area = 0.5 * base* height
+
+print("The area if triangle = ", area , "unit\u00b2")
