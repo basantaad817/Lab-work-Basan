10fee3f2561600621a51cb7a0844ce24dc4b5e99
Basan <basantaad817@gmail.com>
2021-12-01 13:16:57 +0545

    lab 4  done

---
+++ lab4.py
@@ -0,0 +1,15 @@
+# sum of first 10 positive integer
+
+
+# method 1
+
+# a= 0
+# b=11
+# c=range(a,b)
+# d=sum(c)
+# print("The sum of first 10 positive number is ", d)
+
+# method 2
+listnum = [1,2,3,4,5,6,7,8,9,10]
+total = sum(listnum)
+print(total)
