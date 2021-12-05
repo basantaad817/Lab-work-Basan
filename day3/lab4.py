e2785343890031259ee2c5e535b30268e78707b2
Basan <basantaad817@gmail.com>
2021-12-05 13:50:11 +0545

    lab 4 done

---
+++ day3/lab4.py
@@ -0,0 +1,12 @@
+# Given three integers, print the smallest one. (Three integers should be user input) 
+
+num1 = int(input("enter num1 \t"))
+num2 = int(input("enter num2 \t"))
+num3 = int(input("enter num3 \t"))
+
+if num1<num2 and num1<num2:
+    print(num1,", num1 is the smallest")
+elif num2<num1 and num2<num3:
+    print(num2,", num2 is the smallest")
+else:
+    print(num3,",num3 is the smallest")
